6caff81087f15f648b9adbb9d20f8abf442ea415
Federico <federicohv125@gmail.com>
2019-11-28 12:22:55 -0600

    Se reasigna el server que utiliza la aplicacion cinema para que se reconociera el nuevo controlador del login

--- src/main/java/net/fhernandez/cineapp/app/controller/NoticiasController.java
+++ src/main/java/net/fhernandez/cineapp/app/controller/NoticiasController.java
@@ -20,7 +20,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import net.fhernandez.cineapp.app.model.Banner;
 import net.fhernandez.cineapp.app.model.Noticia;
 import net.fhernandez.cineapp.app.services.NoticiaService;
 
