40552dd6501c2ac6cc62f0056f28cea949b4e962
Federico <federicohv125@gmail.com>
2019-11-27 19:46:15 -0600

    Crear controlador login

---
+++ src/main/java/net/fhernandez/cineapp/app/controller/LoginController.java
@@ -0,0 +1,26 @@
+package net.fhernandez.cineapp.app.controller;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class LoginController {
+	
+	//Spring obtiene la sesion actual del objeto request para invalidarla
+	@GetMapping(value = "/logout")
+	public String logout(HttpServletRequest request) {
+		System.out.println("Hola");
+		//Implementacion de Spring security encargada de destruir la sesión.
+		SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+		securityContextLogoutHandler.logout(request, null, null);
+		
+		//Despues de cerrar la sesión redireccionamos al usuario al formulario de login.
+		return "redirect:/login";
+	}
+
+}
