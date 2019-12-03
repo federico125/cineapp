a0a178a414b7b14be9d2b1e02a135508719cf31b
Federico <federicohv125@gmail.com>
2019-12-02 19:50:21 -0600

    Cambios Security tipo autentication BASIC usando Realm USerDatabaseRealm

--- src/main/java/net/fhernandez/cineapp/app/controller/UsuariosController.java
+++ src/main/java/net/fhernandez/cineapp/app/controller/UsuariosController.java
@@ -9,7 +9,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import net.fhernandez.cineapp.app.model.UsuarioPerfil;
 import net.fhernandez.cineapp.app.model.Usuarios;
+import net.fhernandez.cineapp.app.services.UsuarioPerfilService;
+import net.fhernandez.cineapp.app.services.UsuariosService;
 
 @Controller
 @RequestMapping(value="/usuarios")
@@ -17,6 +20,12 @@ public class UsuariosController {
 	
 	@Autowired
 	private BCryptPasswordEncoder code;
+	
+	@Autowired
+	private UsuariosService usuariosService;
+	
+	@Autowired
+	private UsuarioPerfilService usuarioPerfilService;
 	
 	@GetMapping(value ="/demo-Bcrypt")
 	public String PruebaEncriptarContrasena () {
@@ -38,6 +47,20 @@ public class UsuariosController {
 	
 	@PostMapping(value="/save")
 	public String save (@ModelAttribute Usuarios usuarios, @RequestParam("perfil") String perfil) {
+		
+		String passwordTemporal = usuarios.getPassword();
+		String passwordEncriptado = code.encode(passwordTemporal);
+		
+		usuarios.setPassword(passwordEncriptado);
+		usuarios.setEstatus(1);
+		
+		usuariosService.save(usuarios);
+		
+		UsuarioPerfil usuarioPerfil = new UsuarioPerfil();
+		usuarioPerfil.setNombreUsuario(usuarios.getNombreUsuario());
+		usuarioPerfil.setRol(perfil);
+		usuarioPerfilService.save(usuarioPerfil);
+		
 		System.out.println(usuarios);
 		System.out.println(perfil);
 	return "redirect:/usuarios/index";	
